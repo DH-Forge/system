2a77d84ebfcd988bf0b5495d496c4e5a425b2e22
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2025-05-28 01:48:18 +0100

    setup example

--- apps/schema/src/index.ts
+++ apps/schema/src/index.ts
@@ -1,11 +1,13 @@
+import { exampleSchema } from "@forge/models";
 import { Hono } from "hono";
+import { z } from "zod/v4";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-	return c.json({
-		message: "Hello, world!",
-	});
+const exampleJsonSchema = z.toJSONSchema(exampleSchema);
+
+app.get("/example.json", (c) => {
+	return c.json(exampleJsonSchema);
 });
 
 export default app;
