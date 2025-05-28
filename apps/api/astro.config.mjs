08bcafe589caae1f8814d551bdf3e85665e2f0b0
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2025-05-28 23:06:08 +0100

    format

--- apps/api/astro.config.mjs
+++ apps/api/astro.config.mjs
@@ -8,11 +8,11 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://api.dh-forge.com",
-	integrations: [sitemap()],
-
 	server: {
 		port: 4321,
 	},
+
+	integrations: [sitemap()],
 
 	vite: {
 		plugins: [tailwindcss()],
