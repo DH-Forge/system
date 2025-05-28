34bed39c5dcf94ca8f760b4241db5df9ec19cc54
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2025-05-28 23:05:22 +0100

    correct json schema types

--- packages/forge-models/src/version-0.1/character.ts
+++ packages/forge-models/src/version-0.1/character.ts
@@ -1,5 +1,6 @@
 import { z } from "zod/v4";
 
 export const characterSchema = z.object({
+	$schema: z.url(),
 	type: z.literal("character"),
 });
--- packages/forge-models/src/version-0.1/registry-core.ts
+++ packages/forge-models/src/version-0.1/registry-core.ts
@@ -1,5 +1,6 @@
 import { z } from "zod/v4";
 
 export const registryCoreSchema = z.object({
+	$schema: z.url(),
 	type: z.literal("registry-core"),
 });
