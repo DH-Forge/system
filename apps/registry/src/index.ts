9c2af1c2ca497b669d8aa4b82f4825c92af1ec5a
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2025-05-29 14:55:58 +0100

    revert registry to cloudflare workers

--- apps/registry/src/index.ts
+++ apps/registry/src/index.ts
@@ -1,48 +1,9 @@
-import { serve } from "@hono/node-server";
 import { Hono } from "hono";
-import type { z } from "zod/v4";
-import { registryCampaign } from "./v1/campaign";
-import { registryCore } from "./v1/core";
 
-const app = new Hono();
+const app = new Hono<{ Bindings: CloudflareBindings }>();
 
-const versions = ["v1"] as const;
-type Version = (typeof versions)[number];
-
-const modelToFilename = {
-	"core.json": {
-		v1: registryCore,
-	},
-	"campaign.json": {
-		v1: registryCampaign,
-	},
-} satisfies Record<string, Record<Version, z.ZodSafeParseResult<unknown>>>;
-
-const isFilename = (name: string): name is keyof typeof modelToFilename => {
-	return Object.keys(modelToFilename).includes(name);
-};
-const isVersion = (version: string): version is Version => {
-	return versions.includes(version as Version);
-};
-
-app.get("/:version/:fileName", (c) => {
-	const version = c.req.param("version");
+app.get("/core.json", (c) => {
 	const fileName = c.req.param("fileName");
-
-	if (!fileName.endsWith(".json")) {
-		console.warn("File name must end with .json");
-		return c.notFound();
-	}
-
-	if (!isVersion(version)) {
-		console.warn(`Version '${version}' not found`);
-		return c.notFound();
-	}
-
-	if (!isFilename(fileName)) {
-		console.warn(`Endpoint '${fileName}' not found`);
-		return c.notFound();
-	}
 
 	c.header("Server", "dhfs.localhost");
 	c.header("Last-Modified", new Date().toUTCString());
@@ -53,40 +14,10 @@ app.get("/:version/:fileName", (c) => {
 	// Consider re-enabling cache control for production:
 	// c.header("Cache-Control", "public, max-age=3600, must-revalidate"); // e.g., 1 hour cache
 
-	const model = modelToFilename[fileName][version];
-
-	if (!model.success) {
-		c.status(500);
-		return c.json({
-			error: "Internal Server Error",
-			message: `Invalid schema data for '${fileName}'.`,
-		});
-	}
-
-	return c.json(model.data);
-});
-
-const server = serve(
-	{
-		fetch: app.fetch,
-		port: 3000,
-	},
-	(info) => {
-		console.log(`Server is running on http://localhost:${info.port}`);
-	},
-);
-
-// graceful shutdown
-process.on("SIGINT", () => {
-	server.close();
-	process.exit(0);
-});
-process.on("SIGTERM", () => {
-	server.close((err) => {
-		if (err) {
-			console.error(err);
-			process.exit(1);
-		}
-		process.exit(0);
+	return c.json({
+		$schema: "https://dh-forge.com/schema/0.1.x/core.json",
+		type: "character",
 	});
 });
+
+export default app;
